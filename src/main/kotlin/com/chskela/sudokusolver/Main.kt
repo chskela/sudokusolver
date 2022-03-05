8244d5cdec70c8fd9fbffeee89750a6c62e6026e
Chistaykov Aleksey <chskela@gmail.com>
2022-03-05 22:38:13 +0300

    extract fun readInput

--- src/main/kotlin/com/chskela/sudokusolver/Main.kt
+++ src/main/kotlin/com/chskela/sudokusolver/Main.kt
@@ -5,22 +5,25 @@ package com.chskela.sudokusolver
 import java.io.File
 
 fun main() {
-    val input = File("input")
-        .readLines()
-        .withIndex()
-        .flatMap { indexedValue ->
-            val xCoordinate = indexedValue.index
-            indexedValue.value
-                .toCharArray()
-                .withIndex()
-                .filter { it.value != '.' }
-                .map { indexedChar ->
-                    val yCoordinate = indexedChar.index
-                    val number = Character.getNumericValue(indexedChar.value)
-                    (xCoordinate to yCoordinate) to number
-                }
-        }
-        .toMap()
+    val input = readInput()
 
     println(input)
 }
+
+private fun readInput(): Map<Pair<Int, Int>, Int> = File("input")
+    .readLines()
+    .withIndex()
+    .flatMap { indexedValue ->
+        val xCoordinate = indexedValue.index
+        indexedValue.value
+            .toCharArray()
+            .withIndex()
+            .filter { it.value != '.' }
+            .map { indexedChar ->
+                val yCoordinate = indexedChar.index
+                val number = Character.getNumericValue(indexedChar.value)
+                (xCoordinate to yCoordinate) to number
+            }
+    }
+    .toMap()
+
