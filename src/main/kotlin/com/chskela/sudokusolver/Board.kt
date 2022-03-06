839481c793f568a69303bcc04ea9871ec9acdea1
Chistaykov Aleksey <chskela@gmail.com>
2022-03-06 20:05:08 +0300

    Update Board - fix checkRow and checkSquare

--- src/main/kotlin/com/chskela/sudokusolver/Board.kt
+++ src/main/kotlin/com/chskela/sudokusolver/Board.kt
@@ -8,15 +8,17 @@ class Board(
     fun checkSolution(): CheckOutcome = (0..8).fold(CheckOutcome.Ok) { acc, i ->
         val checkL = checkLine(i)
         val checkR = checkRow(i)
+        val checkS = checkSquare(i)
         if (checkL != CheckOutcome.Ok) return@fold checkL
         if (checkR != CheckOutcome.Ok) return@fold checkR
+        if (checkS != CheckOutcome.Ok) return@fold checkS
         acc
     }
 
     private fun checkLine(
         lineNumber: Int,
         coordinateGenerator: (Int) -> Coordinate = { index -> Coordinate(lineNumber, index) }
-    ): CheckOutcome = (0..8).fold(CheckOutcome.Ok) { _, i ->
+    ): CheckOutcome = (0..7).fold(CheckOutcome.Ok) { _, i ->
         val checkedCoordinate = coordinateGenerator(i)
         val checkedValue =
             input[checkedCoordinate] ?: solution[checkedCoordinate] ?: return@fold CheckOutcome.Incomplete
@@ -30,18 +32,12 @@ class Board(
         }
     }
 
+    private fun checkRow(lineNumber: Int) = checkLine(lineNumber) { index -> Coordinate(index, lineNumber) }
 
-    private fun checkRow(
-        lineNumber: Int,
-        coordinateGenerator: (Int) -> Coordinate = { index -> Coordinate(index, lineNumber) }
-    ) = checkLine(lineNumber, coordinateGenerator)
-
-    private fun checkSquare(
-        lineNumber: Int,
-        coordinateGenerator: (Int) -> Coordinate = { index -> Coordinate(index % 3 + lineNumber % 3, lineNumber) }
-    ) = checkLine(
-        lineNumber,
-        coordinateGenerator
-    )
-
+    private fun checkSquare(lineNumber: Int) = checkLine(lineNumber) { index ->
+        Coordinate(
+            (lineNumber / 3) * 3 + index / 3,
+            (lineNumber % 3) * 3 + index % 3
+        )
+    }
 }
